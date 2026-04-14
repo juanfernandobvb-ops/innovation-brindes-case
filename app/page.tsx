e8610064f16cc4990c7b723a78f2938dbfe2e1cc
Juan <juanfernando.bvb@gmail.com>
2026-04-14 18:05:40 -0300

    feat: redirect to login page on home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <div className="flex h-screen items-center justify-center bg-red-500">
-      <h1 className="text-white text-4xl font-bold">
-        Tailwind OK 🚀
-      </h1>
-    </div>
-  )
+  redirect("/login");
 }
